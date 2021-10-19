dd88e33983fc0c3be48a9b0607dd849293bd5f01
Alexis Crespo <acrespo@twilio.com>
2021-10-19 08:25:14 -0500

    Fix eslint warnings and errors

--- index.js
+++ index.js
@@ -42,10 +42,12 @@ function onError(error) {
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
+      // eslint-disable-next-line no-console
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
+      // eslint-disable-next-line no-console
       console.error(`${bind} is already in use`);
       process.exit(1);
       break;
--- test/messaging.spec.js
+++ test/messaging.spec.js
@@ -38,35 +38,31 @@ describe('appointment', () => {
   beforeEach(() => {
     TwilioMock.twiml = Twilio.twiml;
     mockery.registerMock('twilio', TwilioMock);
-    const app = require('../app.js');
+    const app = require('../app');  // eslint-disable-line global-require
     agent = supertest(app);
   });
 
 
   describe('POST /incoming', () => {
-    it('responds to incoming messages', () => {
-      return agent.post('/incoming')
+    it('responds to incoming messages', () =>
+      agent
+        .post('/incoming')
         .send(incomingMMS)
         .expect(200)
-        .expect('<?xml version="1.0" encoding="UTF-8"?><Response>' +
-                '<Message from="+1707XXXXXXX" to="+1707XXXXXXX">' +
-                'Thanks for sending us 1 file(s)</Message></Response>');
-    });
+        .expect(
+          '<?xml version="1.0" encoding="UTF-8"?><Response>' +
+            '<Message from="+1707XXXXXXX" to="+1707XXXXXXX">' +
+            'Thanks for sending us 1 file(s)</Message></Response>',
+        ));
   });
 
   describe('GET /config', () => {
-    it('retrieves the twilio number from the environment config', () => {
-      return agent.get('/config')
-        .expect(200)
-        .expect({ twilioPhoneNumber });
-    });
+    it('retrieves the twilio number from the environment config', () =>
+      agent.get('/config').expect(200).expect({ twilioPhoneNumber }));
   });
 
   describe('GET /images', () => {
-    it('retrieves images receieved via mms', () => {
-      return agent.get('/images')
-        .expect(200)
-        .expect([]);
-    });
+    it('retrieves images receieved via mms', () =>
+      agent.get('/images').expect(200).expect([]));
   });
 });
